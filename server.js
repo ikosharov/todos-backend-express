ba603cc8060a5b7c5e5af1db15e947765b6c6d8a
IKosharov <ivanandr@gmail.com>
2016-04-06 15:10:30 +0300

    adding external web.config.js file

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 
 var Todo = require('./app/models/todo');
+var API_ROOT = require('./web.config');
 
-var API_ROOT = 'mongodb://ivank:ikpass@ds013330.mlab.com:13330/todos-db';
 mongoose.connect(API_ROOT);
 
 // configure app to use bodyParser()
@@ -24,7 +24,9 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
-router.route('/todos').post(function(req, res){
+var todosRoute = router.route('/todos');
+
+todosRoute.post(function(req, res){
   var todo = new Todo();
   todo.title = req.body.title;
 
@@ -33,18 +35,16 @@ router.route('/todos').post(function(req, res){
 
     res.json({ message: 'todo created!' });
   });
-})
-.get(function(req, res){
+});
+
+todosRoute.get(function(req, res){
   Todo.find(function(err, todos) {
     if (err) { res.send(err); }
     res.json(todos);
   });
 });
 
-// test route (GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
+
 
 // more routes
 app.use('/api', router);
---
+++ web.config.js
@@ -0,0 +1,2 @@
+const API_ROOT = 'mongodb://ivank:ikpass@ds013330.mlab.com:13330/todos-db';
+module.exports = API_ROOT;
