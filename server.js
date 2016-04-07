c3c52b7765d261b4bf0606a605f71658b0121f2a
IKosharov <ivanandr@gmail.com>
2016-04-07 16:13:17 +0300

    allowing cors

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var API_ROOT = require('./web.config');
 var router = require('./routes/routes')
 
 var app = express();
+
+// configure app to allow controllers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // configure app to use bodyParser(). this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
