3a4b056444e436c690d39a97266d7121cdf86b54
IKosharov <ivanandr@gmail.com>
2016-07-05 14:07:52 +0300

    changing db location

--- web.config.js
+++ web.config.js
@@ -1,2 +1,2 @@
-module.exports.API_ROOT = 'mongodb://ivank:ikpass@ds013330.mlab.com:13330/todos-db';
+module.exports.API_ROOT = 'mongodb://ivank:ikpass@ds013330.mlab.com:13330/todos-db-v2';
 module.exports.SECRET = 'super.super.secret.string';
