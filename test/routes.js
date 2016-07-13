71054def2ad97372cb1faef3a64b4d7bde5d89b7
IKosharov <ivanandr@gmail.com>
2016-07-13 15:50:34 +0300

    some progress

--- test/routes.js
+++ test/routes.js
@@ -3,25 +3,23 @@ var request = require('supertest');
 var config = require('../web.config.js');
 
 describe('Routing', function () {
-	var url = 'http://todos-backend-express-v2.herokuapp.com';
+	var url = config.PRODUCTION_URL;
 
-	var randomString = function () {
-		var str = Math.random().toString();
-		return str.substr(str.indexOf(".") + 1);
-	}
+	var credentials = {};
 
 	before(function (done) {
-		// setup code goes here						
+		var randomNumStr = Math.random().toString();
+		var randomString = randomNumStr.substr(randomNumStr.indexOf(".") + 1);
+		credentials = {
+			username: 'user' + randomString,
+			password: 'password1'
+		}
+
 		done();
 	});
 
 	describe('auth controller', function () {
 		it('should be able to register users', function (done) {
-			var credentials = {
-				username: 'user' + randomString(),
-				password: 'password1'
-			};
-
 			request(url)
 				.post('/signup')
 				.send(credentials)
@@ -40,7 +38,31 @@ describe('Routing', function () {
 							if (err) {
 								throw err;
 							}
-							
+
+							assert.equal(200, res.status);
+							assert.equal(true, Array.isArray(res.body));
+							done();
+						});
+				});
+		});
+
+		if ('should be able to sign in', function (done) {
+			request(url)
+				.post('/login')
+				.send(credentials)
+				.end(function (err, res) {
+					assert.equal(200, res.status);
+					assert(res.body.token);
+
+					request(url)
+						.get("/api/todos")
+						.set('access_token', res.body.token)
+						.expect('Content-Type', /json/)
+						.end(function (err, res) {
+							if (err) {
+								throw err;
+							}
+
 							assert.equal(200, res.status);
 							assert.equal(true, Array.isArray(res.body));
 							done();
